8a4a7e283d226303da9460bc53ad454d6e9ba5f2
sathya <karpagasathya@gmail.com>
2020-06-14 00:09:16 -0500

    worked on server.js with post and delete

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.static("public"));
-// app.use(express.static("db"));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
- 
+ let storedData = [];
 
   
   
@@ -20,16 +20,14 @@ app.use(express.json());
   });
 
   app.get("/api/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "/db/db.json"));
+    fs.readFile("db/db.json", "utf8", function (err, data) {
+      var savedData = JSON.parse(data);
+      return res.json(savedData);
+    });
   });
 
-  app.get("/api/notes/:id", function (err, res) {
-    notes = JSON.parse(fs.readFile(path.join(__dirname, "/db/db.json"), "utf8"), function (err, data) {
-      if (err) throw err;
-    });
-    res.json(notes[Number(req.params.id)]);
-    // res.json(data);
-  });
+
+  
 
   //  GET '*' - Should return the 'index.html' file
   app.get("*", function (req, res) {
@@ -37,67 +35,58 @@ app.use(express.json());
   });
 
 
+  
+
+
   app.post("/api/notes", function (req, res) {
-    notes = JSON.parse(fs.readFileSync(path.join(__dirname, "/db/db.json"), "utf8"));
-    // data = JSON.parse(notes);
-    newNote = req.body;
-    // req.body.id = notes.length +1;
-    arrayId = notes.length.toString();
-    newNote.id = arrayId;
-    notes.push(newNote);
-    fs.writeFileSync(path.join(__dirname, "/db/db.json"), JSON.stringify(notes), "utf8", function (err) {
+    
+    var newNote = req.body;
+
+    fs.readFile("db/db.json", "utf8", function (err, data) {
       if (err) throw err;
+      storedData = JSON.parse(data);
+      storedData.push(newNote);
+      console.log("newNote added to db.json");
+
+      for (var i = 0; i < storedData.length; i++) {
+        storedData[i].id = parseInt([i]) + 1;
+      }
+
+      fs.writeFile("db/db.json", JSON.stringify(storedData), function (err) {
+        if (err) throw err;
+        console.log("db.json updated");
+      });
+      res.json(true);
     });
-    res.json(notes);
   });
-
-
-  app.delete("/api/notes/:id", function (req, res) {
-    notes = JSON.parse(fs.readFileSync(path.join(__dirname, "/db/db.json"), "utf8"));
-    // notes = JSON.parse(notes);
-    noteId = req.params.id;
-    notes = notes.filter((selectNote) => {
-      return selectNote.id != noteId;
-    });
-    // notes = JSON.stringify(notes);
-    fs.writeFileSync(path.join(__dirname, "/db/db.json"), JSON.stringify(notes), "utf8", function (err) {
-      if (err) throw err;
-    });
-    res.json(notes);
-  });
-
-
-
-
-//  const notes = require("./db/db.json");
-// // let notes = [];
-//   app.get("/api/notes", function (req, res) {
-//     return res.json(notes);
-//   });
 
   
-  // app.post("/api/notes", function (req, res) {
-  //     fs.readFile(path.join(__dirname, "../db/db.json"), (err, data) => {
-  //         if (err) throw err;
-  //         let newNote = req.body;
 
-  //         let notesArr = JSON.parse(data);
-  //         let id = notesArr[notesArr.length - 1].id + 1;
-  //          newNote.id = id;
-  //         notesArr.push(newNote);
-  //         let notesString = JSON.stringify(notesArr);
+  app.delete("/api/notes/:id", function (req, res) {
+    var deleteId = req.params.id;
 
-  //         console.log(typeof notesString);
-  //         fs.writeFileSync(path.join(__dirname, "../db/db.json"), notesString);
+    fs.readFile("db/db.json", "utf8", function (err, data) {
+      if (err) throw err;
+      storedData = JSON.parse(data);
+      storedData.splice(deleteId - 1, 1);
+      console.log("Note " + deleteId + " deleted");
+      
+      for (var i = 0; i < storedData.length; i++) {
+        storedData[i].id = parseInt([i]) + 1;
+      }
 
-  //     })
-  // })
+      fs.writeFile("db/db.json", JSON.stringify(storedData), function (err) {
+        if (err) throw err;
+
+        console.log("db.json updated");
+      });
+      res.json(storedData);
+    });
+  });
 
 
 
 
-
-
-app.listen(PORT, function () {
+app.listen(PORT, function () {                                      
   console.log("App listening on PORT " + PORT);
 });
